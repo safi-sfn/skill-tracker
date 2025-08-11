590840b32b8f8b8dbc0c61ac205884d6c90ac981
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-11 10:32:15 +0530

    feat(main) : skill tacker main app

---
+++ SkillTracker/src/test/java/com/project/skillTrack/SkillTracker/SkillTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.skillTrack.SkillTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
