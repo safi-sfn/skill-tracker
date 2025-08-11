0084b534d9f5ce1cc50df7e0eb8300a8c55a7730
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-11 10:36:47 +0530

    feat(main) : skill tracker main app

---
+++ SkillTracker/src/main/java/com/project/skillTrack/SkillTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.project.skillTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTrackerApplication.class, args);
+		System.out.println("In Main class");
+	}
+
+}
